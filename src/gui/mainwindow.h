f444ae0c0ce942d3e054027a1af4a3010dfe52d1
Georgii Baturin <georgii@snake>
2019-07-06 13:04:25 +0200

    Added small documentaion for MainWindow class.

--- src/gui/mainwindow.h
+++ src/gui/mainwindow.h
@@ -28,35 +28,74 @@
 namespace Ui {
 class MainWindow;
 }
-
+/*!
+ * \class MainWindow
+ * \brief The MainWindow class.
+ * \author arturianec100
+ *
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
     Q_PROPERTY(Project* project READ project)
 public:
+    //! A constructor.
+    /*!
+     * \brief MainWindow.
+     * \param parent is a parent of this object. By default it's a nullptr.
+     * \sa %QMainWindow, %QWidget, <a href=https://wiki.qt.io/Qt_for_Beginners#Parenting_system>Parenting System</a>
+     */
     explicit MainWindow(QWidget *parent = nullptr);
+
+    //! A destructor.
+    /*!
+     * \brief ~MainWindow
+     */
     virtual ~MainWindow() override;
 
+    //! Getter for class member (current project).
+    /*!
+     * \brief project
+     * \return m_pProject
+     * \sa %Project.
+     */
     Project *project() const;
 
+    //TODO: write docs for this function
     virtual void closeEvent(QCloseEvent *event) override;
 
 signals:
     void userSelectedDirOfProjectToOpen(QString dir);
 
 public slots:
+    //! Quit function
+    /*!
+     * \brief quitApplication
+     * Stops threads and initiates exit
+     */
     void quitApplication();
 
+    //! function that opens diagram in given workspace
+    /*!
+     * \brief openDiagram
+     * \param filePath is given workspace in which the work will be performed
+     */
     void openDiagram(QString filePath);
 
+
     void openProjectDialog();
+
+    //! Function that will create new window with application info, like decription, version and contributors.
     void aboutMessageBox();
+
     void updateProjectTree();
 
     void onProjectTreeItemClicked(const QModelIndex &index);
 
 protected:
     void stopWorkerThreads();
+
+    //All setup function bellow will call on startup
 
     void setupMenubar() const;
     void setupToolbar() const;
